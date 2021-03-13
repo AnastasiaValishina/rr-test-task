4db35e22c1960a28560dfbf9c147da6680934d14
AnastasiaValishina <azhidenova@gmail.com>
2021-03-13 14:48:09 +0200

    project upload

---
+++ RR Test Task/Assets/Scripts/Card.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] Image artImage;
+    [SerializeField] Sprite[] artArray;
+    [SerializeField] Text attackText;
+    [SerializeField] Text healthText;
+    [SerializeField] Text manaText;
+
+    [SerializeField] int minValue = -2, maxValue = 9;
+    [SerializeField] int attack = 5;
+    [SerializeField] int health = 5;
+    [SerializeField] int mana = 5;
+    [SerializeField] int[] values = new int[3];
+    void Start()
+    {
+        artImage.sprite = artArray[Random.Range(0, artArray.Length)];
+        values[0] = attack;
+        values[1] = health;
+        values[2] = mana;
+        attackText.text = values[0].ToString();
+        healthText.text = values[1].ToString();
+        manaText.text = values[2].ToString();
+    }
+
+    public void ChangeToRandomValue()
+    {
+        int randomValue = Random.Range(minValue, maxValue);
+        int randomParameter = Random.Range(0, 2);
+        values[randomParameter] = randomValue;
+
+        if (values[1] < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        attackText.text = values[0].ToString();
+        healthText.text = values[1].ToString();
+        manaText.text = values[2].ToString();
+    }
+}
---
+++ RR Test Task/Assets/Scripts/CardSpawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSpawner : MonoBehaviour
+{
+    [SerializeField] int minNumberOfCards = 4, maxNumberOfCards = 6;
+    [SerializeField] Card cardPrefab;
+    [SerializeField] GameObject target;
+
+    [SerializeField] List<Card> cardsAtHand = new List<Card>();
+    int cardIndex = 0;
+
+    void Start()
+    {
+        int numberOfCards = Random.Range(minNumberOfCards, maxNumberOfCards);
+
+        for (int i = 0; i <= numberOfCards; i++)
+        {
+            Card newCard = Instantiate(cardPrefab, transform);
+            cardsAtHand.Add(newCard);
+        }  
+    }
+
+    public void OnButtonClick()
+    {
+        cardsAtHand[cardIndex].ChangeToRandomValue();
+        cardIndex++;
+        if (cardIndex >= cardsAtHand.Count)
+        {
+            cardIndex = 0;
+        }
+    }
+}
